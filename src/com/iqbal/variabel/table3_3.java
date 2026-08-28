f6524e0027847fd1f78b1f6bde3dc3604228e2f4
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-28 21:03:46 +0700

    feat: adding material to Table 3.3 regarding variable data types

---
+++ src/com/iqbal/variabel/table3_3.java
@@ -0,0 +1,15 @@
+package com.iqbal.variabel;
+
+public class table3_3 {
+    public static void main(String[] args) {
+        byte byteNumber = 77;
+        short shortNumber = 777;
+        int intNumber = 77777;
+        long longNumber = 77777777;
+
+        System.out.println(byteNumber);
+        System.out.println(shortNumber);
+        System.out.println(intNumber);
+        System.out.println(longNumber);
+    }
+}
