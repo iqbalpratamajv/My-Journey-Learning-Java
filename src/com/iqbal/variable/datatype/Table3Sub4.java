b99b9c7231dbf4215ec18742fe9c9f416a84b33d
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-28 21:22:27 +0700

    feat: add table 3 sub 4

---
+++ src/com/iqbal/variable/datatype/Table3Sub4.java
@@ -0,0 +1,11 @@
+package com.iqbal.variable.datatype;
+
+public class Table3Sub4 {
+    public static void main(String[] args) {
+        int numberOne = 0337;
+        int numberTwo = 0x1a4;
+
+        System.out.println(numberOne);
+        System.out.println(numberTwo);
+    }
+}
