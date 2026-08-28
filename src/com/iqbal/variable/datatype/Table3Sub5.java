01d84cfb2b3fc13eba97dea8f8f0fb53a630940f
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-28 21:25:27 +0700

    feat: add table 3 sub 5

---
+++ src/com/iqbal/variable/datatype/Table3Sub5.java
@@ -0,0 +1,11 @@
+package com.iqbal.variable.datatype;
+
+public class Table3Sub5 {
+    public static void main(String[] args) {
+        double numberOne = 1038;
+        double numberTwo = 1743.223;
+
+        System.out.println(numberOne);
+        System.out.println(numberTwo);
+    }
+}
