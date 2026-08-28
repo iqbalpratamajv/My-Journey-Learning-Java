f955bbd3fc3773f419f5e75b88af6f840ee5dbf3
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-28 20:57:47 +0700

    feat: adding material to Table 3.2 regarding variable data collection

---
+++ src/com/iqbal/variabel/table3_2.java
@@ -0,0 +1,10 @@
+package com.iqbal.variabel;
+
+public class table3_2 {
+    public static void main(String[] args) {
+        int number = 67;
+        number = 167;
+
+        System.out.println(number);
+    }
+}
