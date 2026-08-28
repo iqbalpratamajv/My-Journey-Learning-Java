57b52b9c204817b81b0dac1698a30fd5d737bc6c
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-28 21:30:20 +0700

    feat: adding character assets

---
+++ src/com/iqbal/variable/datatype/Table3Sub6.java
@@ -0,0 +1,13 @@
+package com.iqbal.variable.datatype;
+
+public class Table3Sub6 {
+    public static void main(String[] args) {
+        char characterOne = 'A';
+        char characterTwo = '\\';
+        char characterThree = '\u0041';
+
+        System.out.println(characterOne);
+        System.out.println(characterTwo);
+        System.out.println(characterThree);
+    }
+}
