b47f52995022b36ec805a282c7f692c070017116
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 13:58:27 +0700

    feat: add operator relational

---
+++ src/com/iqbal/operator/RelationalOperator.java
@@ -0,0 +1,12 @@
+package com.iqbal.operator;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        System.out.println(6 == 6);
+        System.out.println(8 != 4 * 2);
+        System.out.println(7 >= 9);
+        System.out.println(8 <= 10);
+        System.out.println(10 * 8 < 100 + 1);
+        System.out.println(10 > 1);
+    }
+}
