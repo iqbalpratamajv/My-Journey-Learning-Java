b89ffa8337d49990629522f2d71e9c7362a3b62c
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 14:32:06 +0700

    feat: add on one branch material

---
+++ src/com/iqbal/branching/function/oneBranch.java
@@ -0,0 +1,11 @@
+package com.iqbal.branching.function;
+
+public class oneBranch {
+    public static void main(String[] args) {
+        int value = 67;
+
+        if (value <= 100) {
+            System.out.println("Salah");
+        }
+    }
+}
