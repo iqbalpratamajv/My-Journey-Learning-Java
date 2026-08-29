db609156214e715b93fee30218e2ca0ca2cdd99d
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 09:54:10 +0700

    fix: fix name class

--- src/com/iqbal/variable/datatype/CharacterDataType.java
+++ src/com/iqbal/variable/datatype/CharacterDataType.java
@@ -1,6 +1,6 @@
 package com.iqbal.variable.datatype;
 
-public class CharacterVariable {
+public class CharacterDataType {
     public static void main(String[] args) {
         char characterOne = 'A';
         char characterTwo = '\\';
