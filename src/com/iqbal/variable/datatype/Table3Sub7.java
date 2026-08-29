4662ef7f199be35eb9c6f4a4080b6974e6942ad1
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 07:55:47 +0700

    feat: adding material on converting char values ​​to int

---
+++ src/com/iqbal/variable/datatype/Table3Sub7.java
@@ -0,0 +1,10 @@
+package com.iqbal.variable.datatype;
+
+public class Table3Sub7 {
+    public static void main(String[] args) {
+        char character = 'A';
+        int conversion = (int) character;
+
+        System.out.println(conversion);
+    }
+}
