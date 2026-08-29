671727c62cf57da0aa82f32dc1bcc2872a15ef3e
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 14:23:57 +0700

    feat: Add material on assignment operators

---
+++ src/com/iqbal/operator/AssignmentOperator.java
@@ -0,0 +1,12 @@
+package com.iqbal.operator;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        int numberTwo = 15;
+
+        numberOne += numberTwo;
+        System.out.println(numberOne);
+    }
+}
