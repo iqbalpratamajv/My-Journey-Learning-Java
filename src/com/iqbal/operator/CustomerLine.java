8a92d9a1e87b98c90b10b238bf297caf8798add2
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 13:53:36 +0700

    feat: Add a buyer queue project using the increment method

---
+++ src/com/iqbal/operator/CustomerLine.java
@@ -0,0 +1,11 @@
+package com.iqbal.operator;
+
+public class CustomerLine {
+    public static void main(String[] args) {
+
+        int queueNumber = 0;
+
+        System.out.println("Pelanggan pertama: " + ++queueNumber);
+        System.out.println("Pelanggan kedua: " + ++queueNumber);
+    }
+}
