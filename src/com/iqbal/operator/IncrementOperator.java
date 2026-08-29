ddf5addc775ad31288c4f5422071e169fe7fbd5b
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 10:13:01 +0700

    feat: add increment material

---
+++ src/com/iqbal/operator/IncrementOperator.java
@@ -0,0 +1,19 @@
+package com.iqbal.operator;
+
+public class IncrementOperator {
+    public static void main(String[] args) {
+        int numberA = 1;
+        System.out.println("numberA = " + numberA);
+        System.out.println("++numberA = " + ++numberA);
+
+        System.out.println();
+        int numberB = 1;
+        System.out.println("NumberB = " + numberB);
+        System.out.println("NumberB++ = " + ++numberB);
+
+        System.out.println();
+        int numberC = 1;
+        System.out.println("NumberC = " + numberC);
+        System.out.println("NumberC-- = " + --numberC);
+    }
+}
