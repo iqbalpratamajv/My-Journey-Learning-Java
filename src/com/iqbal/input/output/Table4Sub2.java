0d65c5dfbe5aaa22ecfffeda06790c6f4c00fa43
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 08:13:51 +0700

    feat: adding input output material

---
+++ src/com/iqbal/input/output/Table4Sub2.java
@@ -0,0 +1,17 @@
+package com.iqbal.input.output;
+
+import java.util.Scanner;
+
+public class Table4Sub2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan bilangan pertama: ");
+        int numberOne = input.nextInt();
+
+        System.out.print("Masukkan bilangan kedua: ");
+        int numberTwo = input.nextInt();
+
+        System.out.println("Bilangan pertama: " + numberOne + "\nBilangan kedua: " + numberTwo);
+    }
+}
