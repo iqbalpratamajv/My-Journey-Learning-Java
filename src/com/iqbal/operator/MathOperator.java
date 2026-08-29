febe2a21acb1b9d1f12d26284d520c6b4349c051
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 09:26:21 +0700

    feat: adding operator number

---
+++ src/com/iqbal/operator/MathOperator.java
@@ -0,0 +1,19 @@
+package com.iqbal.operator;
+
+public class MathOperator {
+    public static void main(String[] args) {
+
+        int add = 1 + 7;
+        int subtract = 1 - 7;
+        int multiply = 7 * 1;
+        int divide = 24 / 2;
+        int remainder = 24 % 7;
+
+        System.out.println(add);
+        System.out.println(subtract);
+        System.out.println(multiply);
+        System.out.println(divide);
+        System.out.println(remainder);
+
+    }
+}
