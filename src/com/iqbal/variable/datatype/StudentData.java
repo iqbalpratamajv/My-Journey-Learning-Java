096632aa31369a69b66b85646ee2ed6541eb0ef2
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-29 08:05:33 +0700

    feat:Adding the file for the variables lesson material and the file for the variables and data types practical exercise.

---
+++ src/com/iqbal/variable/datatype/StudentData.java
@@ -0,0 +1,13 @@
+package com.iqbal.variable.datatype;
+
+public class StudentData {
+    public static void main(String[] args) {
+        String name = "Iqbal Dwi Pratama";
+        String nim = "225140293";
+        String room = "2ITA2";
+
+        System.out.println("Name : " + name);
+        System.out.println("NIM  : " + nim);
+        System.out.println("Class: " + room);
+    }
+}
---
+++ src/com/iqbal/variable/datatype/Table3Sub8.java
@@ -0,0 +1,11 @@
+package com.iqbal.variable.datatype;
+
+public class Table3Sub8 {
+    public static void main(String[] args) {
+        String name = "Iqbal Dwi Pratama";
+        String hello = "Selamat datang\"Iqbal Dwi Pratama\"";
+
+        System.out.println(name);
+        System.out.println(hello);
+    }
+}
