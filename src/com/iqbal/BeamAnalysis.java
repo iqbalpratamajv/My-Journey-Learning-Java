6341724b6c39df7677f4bb73df23c3533db7c6a2
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-31 14:18:13 +0700

    feat: add beem analysis

---
+++ src/com/iqbal/BeamAnalysis.java
@@ -0,0 +1,23 @@
+package com.iqbal;
+
+import java.util.Scanner;
+
+public class BeamAnalysis {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang balok: ");
+        int lenghtOfTheBlock = input.nextInt();
+
+        System.out.print("Masukkan lebar balok: ");
+        int blockWidth = input.nextInt();
+
+        System.out.print("Masukkan tinggi balok: ");
+        int blockHeight = input.nextInt();
+
+        int surfaceAreaOfBlock = 2 * ((lenghtOfTheBlock * blockWidth) + (lenghtOfTheBlock * blockHeight) + (blockWidth * blockHeight));
+
+        System.out.println("Hasil dari luas permukaan balok: " + surfaceAreaOfBlock);
+        input.close();
+    }
+}
