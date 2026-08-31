11b3dad18f493a81200984d2e4c6c3fd66a3c1bc
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-08-31 14:04:11 +0700

    feat: add all operator material

---
+++ src/com/iqbal/operator/AllOperator.java
@@ -0,0 +1,15 @@
+package com.iqbal.operator;
+
+public class AllOperator {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 5;
+
+        System.out.println("Operasi Aritmatika");
+        System.out.println("Penjumlahan: " + (numberOne + numberTwo));
+        System.out.println("Pengurangan: " + (numberOne - numberTwo));
+        System.out.println("Perkalian: " + (numberOne * numberTwo));
+        System.out.println("Pembagian: " + (numberOne / numberTwo));
+        System.out.println("Modulus: " + (numberOne % numberTwo));
+    }
+}
