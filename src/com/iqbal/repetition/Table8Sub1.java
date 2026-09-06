1098acee99d442ada03b7b0f07f2d4e8f38bad42
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-09-07 04:56:09 +0700

    feat: add repetition material

---
+++ src/com/iqbal/repetition/Table8Sub1.java
@@ -0,0 +1,9 @@
+package com.iqbal.repetition;
+
+public class Table8Sub1 {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10; a++) {
+            System.out.println("Selamat datang");
+        }
+    }
+}
