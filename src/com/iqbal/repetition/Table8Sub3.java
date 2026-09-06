7e65f73c5bc1644fb77e339575318af6ca2bd922
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-09-07 05:06:53 +0700

    feat: add table 8 sub 3

---
+++ src/com/iqbal/repetition/Table8Sub3.java
@@ -0,0 +1,16 @@
+package com.iqbal.repetition;
+
+public class Table8Sub3 {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 20; a += 2) {
+            System.out.print(a + " ");
+        }
+
+        System.out.println();
+
+        for (int a = 20; a >= 1; a -= 2) {
+            System.out.print(a + " ");
+        }
+    }
+
+}
