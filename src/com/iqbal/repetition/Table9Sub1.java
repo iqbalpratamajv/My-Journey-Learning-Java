ca9d268b694f73cfaac29756a76a5f8f17850d7b
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-09-07 19:04:45 +0700

    feat: add while material

---
+++ src/com/iqbal/repetition/Table9Sub1.java
@@ -0,0 +1,13 @@
+package com.iqbal.repetition;
+
+public class Table9Sub1 {
+    public static void main(String[] args) {
+        int initialValue = 1;
+        int finalValue = 20;
+
+        while (initialValue <= finalValue) {
+            System.out.print(initialValue + " ");
+            initialValue++;
+        }
+    }
+}
