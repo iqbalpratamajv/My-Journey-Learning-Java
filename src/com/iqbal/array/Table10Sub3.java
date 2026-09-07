9ac778771f6867973d44ca62bbf14e4c56865f8d
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-09-07 19:23:52 +0700

    feat: add array material

---
+++ src/com/iqbal/array/Table10Sub3.java
@@ -0,0 +1,11 @@
+package com.iqbal.array;
+
+public class Table10Sub3 {
+    public static void main(String[] args) {
+        int[] lotteryNumber = {59, 90, 100, 89, 78, 90, 34, 78};
+
+        for (int lottery : lotteryNumber) {
+            System.out.print(lottery + " ");
+        }
+    }
+}
