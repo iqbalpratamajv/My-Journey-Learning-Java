6ba7a77a57ec873507dfd704e781a53ef7fa3f1b
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-09-21 18:26:14 +0700

    feat: belajar method menampilkan kelayar menggunakan void

---
+++ src/com/iqbal/method/StudyMethod.java
@@ -0,0 +1,12 @@
+package com.iqbal.method;
+
+public class StudyMethod {
+
+    static void tampilkanKelayar() {
+        System.out.println("Hallo semua");
+    }
+
+    public static void main(String[] args) {
+        tampilkanKelayar();
+    }
+}
