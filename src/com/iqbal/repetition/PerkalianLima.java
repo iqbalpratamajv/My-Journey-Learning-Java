f464e98555f7eec7b7c28e1eef251e15427087c9
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-09-23 15:06:03 +0700

    feat: belajar perulangan for dengan studi kasus perkalian lima

---
+++ src/com/iqbal/repetition/PerkalianLima.java
@@ -0,0 +1,11 @@
+package com.iqbal.repetition;
+
+public class PerkalianLima {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            int hasil = 5 * i;
+
+            System.out.println("Hasil perkalian 5 " + "x " + i + " = " + hasil);
+        }
+    }
+}
