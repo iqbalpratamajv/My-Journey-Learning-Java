14abef1d427fbba42f68f21f63e9a30373462a8c
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-09-23 15:39:28 +0700

    feat: belajar faktorial

--- src/com/iqbal/repetition/FaktorialTiga.java
+++ src/com/iqbal/repetition/FaktorialTiga.java
@@ -2,12 +2,13 @@ package com.iqbal.repetition;
 
 public class FaktorialTiga {
     public static void main(String[] args) {
+
         int total = 1;
 
         for (int i = 3; i >= 1; i--) {
             total *= i;
-
-            System.out.println("Hasilnnya: " + total);
         }
+        System.out.println(total);
+
     }
 }
